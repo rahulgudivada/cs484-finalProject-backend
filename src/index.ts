63537ed3073b71e92529d25b596e1bb0351d8f1b
gudivadarahul <rgudiv3.uic.edu>
2021-12-09 17:21:02 -0600

    port fail

--- src/index.ts
+++ src/index.ts
@@ -133,6 +133,6 @@ app.get("/auth/logout", (req, res) => {
   }
 })
 
-app.listen(process.env.PORT || 4000, () => {
+app.listen(process.env.PORT || '0.0.0.0', () => {
   console.log("Server started")
 })
